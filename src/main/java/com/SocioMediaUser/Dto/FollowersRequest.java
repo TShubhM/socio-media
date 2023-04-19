3c5ee22365f2732dad1fd8dfdd7b15ae8d2449ac
Saralajunare-1832 <saralajunare93@gmail.com>
2023-04-19 11:06:40 +0530

    followersrequest and followingreequest entity added

---
+++ src/main/java/com/SocioMediaUser/Dto/FollowersRequest.java
@@ -0,0 +1,15 @@
+package com.SocioMediaUser.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FollowersRequest {
+    String followingUsername;
+    String followedUsername;
+}
+
+
